9a116003b0cad5e0a3e20f3593633375d8c195c9
Richard Sandiford <richard.sandiford@linaro.org>
2016-09-07 13:52:47 +0100

    [17/77] Add an int_mode_for_size helper function
    This patch adds a wrapper around mode_for_size for cases in which
    the mode class is MODE_INT (the commonest case).  The return type
    can then be an opt_scalar_int_mode instead of a machine_mode.
    
    2017-07-13  Richard Sandiford  <richard.sandiford@linaro.org>
    	    Alan Hayward  <alan.hayward@arm.com>
    	    David Sherwood  <david.sherwood@arm.com>
    
    gcc/
    	* machmode.h (int_mode_for_size): New function.
    	* builtins.c (set_builtin_user_assembler_name): Use int_mode_for_size
    	instead of mode_for_size.
    	* calls.c (save_fixed_argument_area): Likewise.  Make use of BLKmode
    	explicit.
    	* combine.c (expand_field_assignment): Use int_mode_for_size
    	instead of mode_for_size.
    	(make_extraction): Likewise.
    	(simplify_shift_const_1): Likewise.
    	(simplify_comparison): Likewise.
    	* dojump.c (do_jump): Likewise.
    	* dwarf2out.c (mem_loc_descriptor): Likewise.
    	* emit-rtl.c (init_derived_machine_modes): Likewise.
    	* expmed.c (flip_storage_order): Likewise.
    	(convert_extracted_bit_field): Likewise.
    	* expr.c (copy_blkmode_from_reg): Likewise.
    	* graphite-isl-ast-to-gimple.c (max_mode_int_precision): Likewise.
    	* internal-fn.c (expand_mul_overflow): Likewise.
    	* lower-subreg.c (simple_move): Likewise.
    	* optabs-libfuncs.c (init_optabs): Likewise.
    	* simplify-rtx.c (simplify_unary_operation_1): Likewise.
    	* tree.c (vector_type_mode): Likewise.
    	* tree-ssa-strlen.c (handle_builtin_memcmp): Likewise.
    	* tree-vect-data-refs.c (vect_lanes_optab_supported_p): Likewise.
    	* tree-vect-generic.c (expand_vector_parallel): Likewise.
    	* tree-vect-stmts.c (vectorizable_load): Likewise.
    	(vectorizable_store): Likewise.
    
    gcc/ada/
    	* gcc-interface/decl.c (gnat_to_gnu_entity): Use int_mode_for_size
    	instead of mode_for_size.
    	(gnat_to_gnu_subprog_type): Likewise.
    	* gcc-interface/utils.c (make_type_from_size): Likewise.

--- gcc/tree-vect-stmts.c
+++ gcc/tree-vect-stmts.c
@@ -6021,7 +6021,7 @@ vectorizable_store (gimple *stmt, gimple_stmt_iterator *gsi, gimple **vec_stmt,
 		     supported.  */
 		  unsigned lsize
 		    = group_size * GET_MODE_BITSIZE (elmode);
-		  elmode = mode_for_size (lsize, MODE_INT, 0);
+		  elmode = *int_mode_for_size (lsize, 0);
 		  vmode = mode_for_vector (elmode, nunits / group_size);
 		  /* If we can't construct such a vector fall back to
 		     element extracts from the original vector type and
@@ -7061,7 +7061,7 @@ vectorizable_load (gimple *stmt, gimple_stmt_iterator *gsi, gimple **vec_stmt,
 		     to a larger load.  */
 		  unsigned lsize
 		    = group_size * TYPE_PRECISION (TREE_TYPE (vectype));
-		  elmode = mode_for_size (lsize, MODE_INT, 0);
+		  elmode = *int_mode_for_size (lsize, 0);
 		  vmode = mode_for_vector (elmode, nunits / group_size);
 		  /* If we can't construct such a vector fall back to
 		     element loads of the original vector type.  */
