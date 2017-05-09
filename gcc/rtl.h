1b69c3c5f154f2638aa8210d512fc2eeee6f8569
Richard Sandiford <richard.sandiford@linaro.org>
2017-05-09 12:31:55 +0100

    poly_int: GET_MODE_SIZE
    This patch changes GET_MODE_SIZE from unsigned char to
    poly_uint16.  (8 bits is no longer enough with 2048-bit
    SVE vectors.)

--- gcc/rtl.h
+++ gcc/rtl.h
@@ -2203,7 +2203,7 @@ extern void get_full_rtx_cost (rtx, machine_mode, enum rtx_code, int,
 			       struct full_rtx_costs *);
 extern poly_int64 subreg_lsb (const_rtx);
 extern poly_int64 subreg_lsb_1 (machine_mode, machine_mode, poly_int64);
-extern poly_int64 subreg_size_offset_from_lsb (unsigned int, unsigned int,
+extern poly_int64 subreg_size_offset_from_lsb (poly_int64, poly_int64,
 					       poly_int64);
 
 /* Return the subreg byte offset for a subreg whose outer mode is
@@ -2842,8 +2842,9 @@ extern rtx operand_subword (rtx, poly_int64, int, machine_mode);
 extern rtx operand_subword_force (rtx, poly_int64, machine_mode);
 extern machine_mode wider_subreg_mode (machine_mode, machine_mode);
 extern machine_mode wider_subreg_mode (const_rtx);
+extern machine_mode narrower_subreg_mode (machine_mode, machine_mode);
 extern int subreg_lowpart_p (const_rtx);
-extern poly_int64 subreg_size_lowpart_offset (unsigned int, unsigned int);
+extern poly_int64 subreg_size_lowpart_offset (poly_int64, poly_int64);
 
 /* Return true if a subreg of mode OUTERMODE would only access part of
    an inner register with mode INNERMODE.  The other bits of the inner
@@ -2907,7 +2908,7 @@ subreg_lowpart_offset (machine_mode outermode, machine_mode innermode)
 				     GET_MODE_SIZE (innermode));
 }
 
-extern poly_int64 subreg_size_highpart_offset (unsigned int, unsigned int);
+extern poly_int64 subreg_size_highpart_offset (poly_int64, poly_int64);
 
 /* Return the SUBREG_BYTE for an OUTERMODE highpart of an INNERMODE value.  */
 
